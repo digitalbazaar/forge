ee878a6a5f4e98dc8b7c7217cf8e13654b55f38d
Stefan Siegl <stesie@brokenpipe.de>
2012-07-05 01:23:42 +0200

    Add PKCS#12 SafeContents decryption (PBE based).

--- tests/nodeunit/pkcs12.js
+++ tests/nodeunit/pkcs12.js
@@ -136,3 +136,31 @@ exports.testGenerateKey = function(test) {
   test.done();
 };
 
+exports.testPkcs12FromAsn1_EncryptedMix = function(test) {
+  var p12Der = fs.readFileSync(__dirname + '/_files/pkcs12_encmixed.p12', 'binary');
+  var p12Asn1 = forge.asn1.fromDer(p12Der);
+
+  var p12 = forge.pkcs12.pkcs12FromAsn1(p12Asn1, '123456');
+  test.equals(p12.version, 3);
+
+  /* The PKCS#12 PFX has two SafeContents instances,
+     the first one is *not* encrypted, but contains two shrouded keys,
+     the second one is encrypted and has six certificates in it. */
+  test.equals(p12.safeContents.length, 2);
+
+  test.equals(p12.safeContents[0].encrypted, false);
+  test.equals(p12.safeContents[0].safeBags.length, 2);
+  test.equals(p12.safeContents[0].safeBags[0].type, forge.pki.oids.pkcs8ShroudedKeyBag);
+  test.equals(p12.safeContents[0].safeBags[1].type, forge.pki.oids.pkcs8ShroudedKeyBag);
+
+  test.equals(p12.safeContents[1].encrypted, true);
+  test.equals(p12.safeContents[1].safeBags.length, 6);
+  test.equals(p12.safeContents[1].safeBags[0].type, forge.pki.oids.certBag);
+  test.equals(p12.safeContents[1].safeBags[1].type, forge.pki.oids.certBag);
+  test.equals(p12.safeContents[1].safeBags[2].type, forge.pki.oids.certBag);
+  test.equals(p12.safeContents[1].safeBags[3].type, forge.pki.oids.certBag);
+  test.equals(p12.safeContents[1].safeBags[4].type, forge.pki.oids.certBag);
+  test.equals(p12.safeContents[1].safeBags[5].type, forge.pki.oids.certBag);
+
+  test.done();
+}
