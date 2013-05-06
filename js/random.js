digitalbazaar/forge

commit 85b6db9b44f7dc2e7b3f732d4ca31c5010bee88b

Always use strong source of entropy if available.
- Use window.crypto.getRandomValues directly in prng when
  reseeding if there is not enough data in first pool.
- Do not collect potentially weaker sources of entropy if
  window.crypto.getRandomValues is available, though Fortuna
  should offer some protection against an attack here?
- Default (random.js) plugin relies solely on
  window.crypto.getRandomValues if available now; this is
  presumably the behavior apps using JS will take.

js/random.js
@@ -63,59 +63,48 @@ prng_aes.md = forge.md.sha1;
 // create default prng context
 var _ctx = forge.prng.create(prng_aes);
 
-// get load time entropy
-_ctx.collectInt(+new Date(), 32);
+// add other sources of entropy only if window.crypto.getRandomValues is not
+// available -- otherwise this source will be automatically used by the prng
+if(!(window && window.crypto && window.crypto.getRandomValues)) {
+  // get load time entropy
+  _ctx.collectInt(+new Date(), 32);
 
-// add some entropy from navigator object
-if(typeof(navigator) !== 'undefined') {
-  var _navBytes = '';
-  for(var key in navigator) {
-    try {
-      if(typeof(navigator[key]) == 'string') {
-        _navBytes += navigator[key];
+  // add some entropy from navigator object
+  if(typeof(navigator) !== 'undefined') {
+    var _navBytes = '';
+    for(var key in navigator) {
+      try {
+        if(typeof(navigator[key]) == 'string') {
+          _navBytes += navigator[key];
+        }
       }
-    } catch(e) {
-      /* Some navigator keys might not be accessible, e.g. the geolocation
-         attribute throws an exception if touched in Mozilla chrome:// context.
+      catch(e) {
+        /* Some navigator keys might not be accessible, e.g. the geolocation
+          attribute throws an exception if touched in Mozilla chrome://
+          context.
 
-         Silently ignore this and just don't use this as a source of entropy. */
+          Silently ignore this and just don't use this as a source of
+          entropy. */
+      }
     }
+    _ctx.collect(_navBytes);
+    _navBytes = null;
   }
-  _ctx.collect(_navBytes);
-  _navBytes = null;
-}
 
-// window.crypto.getRandomValues is a strong source of randomness if available
-if (window && window.crypto && window.crypto.getRandomValues) {
-  // completely fill the generator: 32 pools, each of which wants 32 bytes of data
-  // TODO: is this a good target?
-  var ENTROPY_BYTES = 32*32;
-  var entropy = new Uint32Array(ENTROPY_BYTES/4);
-  try {
-    window.crypto.getRandomValues(entropy);
-    for (var i = 0; i < entropy.length; i++) {
-      _ctx.collectInt(entropy[i], 32);
-    }
-  } catch (e) {
-    // Mozilla claims getRandomValues can throw QuotaExceededError, so ignore errors
-    // https://developer.mozilla.org/en-US/docs/DOM/window.crypto.getRandomValues
-    // However I've never observed this exception
+  // add mouse and keyboard collectors if jquery is available
+  if($) {
+    // set up mouse entropy capture
+    $().mousemove(function(e) {
+      // add mouse coords
+      _ctx.collectInt(e.clientX, 16);
+      _ctx.collectInt(e.clientY, 16);
+    });
+
+    // set up keyboard entropy capture
+    $().keypress(function(e) {
+      _ctx.collectInt(e.charCode, 8);
+    });
   }
-}
-
-// add mouse and keyboard collectors if jquery is available
-if($) {
-  // set up mouse entropy capture
-  $().mousemove(function(e) {
-    // add mouse coords
-    _ctx.collectInt(e.clientX, 16);
-    _ctx.collectInt(e.clientY, 16);
-  });
-
-  // set up keyboard entropy capture
-  $().keypress(function(e) {
-    _ctx.collectInt(e.charCode, 8);
-  });
 }
 
 /* Random API */

js/prng.js
@@ -101,34 +101,58 @@ prng.create = function(plugin) {
    * Private function that reseeds a generator.
    */
   function _reseed() {
-    // not enough seed data... but we need to get going so just
-    // be sad and add some weak random data
+    // not enough seed data...
+    var reseeds = ctx.reseeds;
     if(ctx.pools[0].messageLength < 32) {
-      /* Draws from Park-Miller "minimal standard" 31 bit PRNG,
-      implemented with David G. Carta's optimization: with 32 bit math
-      and without division (Public Domain). */
-      var needed = (32 - ctx.pools[0].messageLength) << 5;
-      var b = '';
-      var hi, lo, next;
-      var seed = Math.floor(Math.random() * 0xFFFF);
-      while(b.length < needed) {
-        lo = 16807 * (seed & 0xFFFF);
-        hi = 16807 * (seed >> 16);
-        lo += (hi & 0x7FFF) << 16;
-        lo += hi >> 15;
-        lo = (lo & 0x7FFFFFFF) + (lo >> 31);
-        seed = lo & 0xFFFFFFFF;
-
-        // consume lower 3 bytes of seed
-        for(var i = 0; i < 3; ++i) {
-          // throw in more pseudo random
-          next = seed >>> (i << 3);
-          next ^= Math.floor(Math.random() * 0xFF);
-          b += String.fromCharCode(next & 0xFF);
+      // use window.crypto.getRandomValues strong source of entropy if
+      // available
+      if(window && window.crypto && window.crypto.getRandomValues) {
+        var needed = (32 - ctx.pools[0].messageLength) << 5;
+        var entropy = new Uint32Array(needed / 4);
+        try {
+          window.crypto.getRandomValues(entropy);
+          for(var i = 0; i < entropy.length; ++i) {
+            // will automatically reseed in collect
+            ctx.collectInt(entropy[i], 32);
+          }
+        }
+        catch (e) {
+          /* Mozilla claims getRandomValues can throw QuotaExceededError, so
+           ignore errors. In this case, weak entropy will be added, but
+           hopefully this never happens.
+           https://developer.mozilla.org/en-US/docs/DOM/window.crypto.getRandomValues
+           However I've never observed this exception --@evanj */
         }
       }
-      // will automatically reseed in collect
-      ctx.collect(b);
+
+      // be sad and add some weak random data
+      if(reseeds === ctx.reseeds && ctx.pools[0].messageLength < 32) {
+        /* Draws from Park-Miller "minimal standard" 31 bit PRNG,
+        implemented with David G. Carta's optimization: with 32 bit math
+        and without division (Public Domain). */
+        var needed = (32 - ctx.pools[0].messageLength) << 5;
+        var b = '';
+        var hi, lo, next;
+        var seed = Math.floor(Math.random() * 0xFFFF);
+        while(b.length < needed) {
+          lo = 16807 * (seed & 0xFFFF);
+          hi = 16807 * (seed >> 16);
+          lo += (hi & 0x7FFF) << 16;
+          lo += hi >> 15;
+          lo = (lo & 0x7FFFFFFF) + (lo >> 31);
+          seed = lo & 0xFFFFFFFF;
+
+          // consume lower 3 bytes of seed
+          for(var i = 0; i < 3; ++i) {
+            // throw in more pseudo random
+            next = seed >>> (i << 3);
+            next ^= Math.floor(Math.random() * 0xFF);
+            b += String.fromCharCode(next & 0xFF);
+          }
+        }
+        // will automatically reseed in collect
+        ctx.collect(b);
+      }
     }
     else {
       // create a SHA-1 message digest

commit 2573bb2960d2c22c1414ae1fca0a7d4bbd5d85f5

random.js: Use typeof window to avoid ReferenceError on Node.
Fixes Node bug introduced by 85b6db9b44 and b2d19fb1ac

js/random.js
@@ -65,7 +65,7 @@ var _ctx = forge.prng.create(prng_aes);
 
 // add other sources of entropy only if window.crypto.getRandomValues is not
 // available -- otherwise this source will be automatically used by the prng
-if(!(window && window.crypto && window.crypto.getRandomValues)) {
+if(!(typeof window !== 'undefined' && window.crypto && window.crypto.getRandomValues)) {
   // get load time entropy
   _ctx.collectInt(+new Date(), 32);
 
@@ -122,6 +122,14 @@ forge.random = forge.random || {};
 forge.random.getBytes = function(count) {
   return _ctx.generate(count);
 };
+
+/**
+ * Adds bytes of entropy to the random number generator. Use this to provide
+ * high-quality entropy, if it is available from some other source.
+ */
+forge.random.seed = function(bytes) {
+  return _ctx.collect(bytes);
+}
 
 })(typeof(jQuery) !== 'undefined' ? jQuery : null);
 