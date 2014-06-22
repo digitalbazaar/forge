fc6d046067886fe17f28a53081d0fbb7dd456c16
Dave Longley <dlongley@digitalbazaar.com>
2014-06-22 17:02:00 -0400

    Fix formatting.

--- nodejs/test/kem.js
+++ nodejs/test/kem.js
@@ -5,15 +5,11 @@ function Tests(ASSERT, KEM, MD, RSA, UTIL, JSBN, RANDOM) {
   function FixedSecureRandom(str) {
     var _str = UTIL.hexToBytes(str);
     var index = 0;
-
-
-    this.getBytes = this.getBytesSync = function(count, callback) {
-      var r = _str.substr(index,count);
-
+    this.getBytesSync = function(count) {
+      var r = _str.substr(index, count);
       index += count;
-
       return r;
-    }
+    };
   }
 
   describe('kem', function() {
@@ -25,7 +21,6 @@ function Tests(ASSERT, KEM, MD, RSA, UTIL, JSBN, RANDOM) {
 
       var result = kem.encrypt(pair.publicKey, 256);
       var key1 = result.key;
-
       var key2 = kem.decrypt(pair.privateKey, result.ciphertext, 256);
 
       ASSERT.equal(key1, key2);
@@ -38,7 +33,6 @@ function Tests(ASSERT, KEM, MD, RSA, UTIL, JSBN, RANDOM) {
    */
   describe('C.6 Test vectors for RSA-KEM from ISO-18033-2 final', function() {
     it('should pass test vector C.6.1', function() {
-
       var n = '5888113332502691251761936431009284884966640757179802337490546478326238537107326596800820237597139824869184990638749556269785797065508097452399642780486933';
       var e = '65537';
       var d = '3202313555859948186315374524474173995679783580392140237044349728046479396037520308981353808895461806395564474639124525446044708705259675840210989546479265';
@@ -47,13 +41,13 @@ function Tests(ASSERT, KEM, MD, RSA, UTIL, JSBN, RANDOM) {
       var K = '5f8de105b5e96b2e490ddecbd147dd1def7e3b8e0e6a26eb7b956ccb8b3bdc1ca975bc57c3989e8fbad31a224655d800c46954840ff32052cdf0d640562bdfadfa263cfccf3c52b29f2af4a1869959bc77f854cf15bd7a25192985a842dbff8e13efee5b7e7e55bbe4d389647c686a9a9ab3fb889b2d7767d3837eea4e0a2f04';
 
       var kdf = new KEM.kdf1(MD.sha1.create());
-
       var rnd = new FixedSecureRandom('032e45326fa859a72ec235acff929b15d1372e30b207255f0611b8f785d764374152e0ac009e509e7ba30cd2f1778e113b64e135cf4e2292c75efe5288edfda4');
-
       var kem = KEM.rsa.create(kdf, {rng: rnd});
 
-      var rsaPublicKey = RSA.setPublicKey(new JSBN.BigInteger(n), new JSBN.BigInteger(e));
-      var rsaPrivateKey = RSA.setPrivateKey(new JSBN.BigInteger(n),null , new JSBN.BigInteger(d));
+      var rsaPublicKey = RSA.setPublicKey(
+        new JSBN.BigInteger(n), new JSBN.BigInteger(e));
+      var rsaPrivateKey = RSA.setPrivateKey(
+        new JSBN.BigInteger(n), null, new JSBN.BigInteger(d));
 
       var result = kem.encrypt(rsaPublicKey, 128);
       ASSERT.equal(UTIL.bytesToHex(result.ciphertext), C0);
@@ -61,11 +55,9 @@ function Tests(ASSERT, KEM, MD, RSA, UTIL, JSBN, RANDOM) {
 
       var decryptedKey = kem.decrypt(rsaPrivateKey, result.ciphertext, 128);
       ASSERT.equal(UTIL.bytesToHex(decryptedKey), K);
-
     });
 
     it('should pass test vector C.6.2', function() {
-
       var n = '5888113332502691251761936431009284884966640757179802337490546478326238537107326596800820237597139824869184990638749556269785797065508097452399642780486933';
       var e = '65537';
       var d = '3202313555859948186315374524474173995679783580392140237044349728046479396037520308981353808895461806395564474639124525446044708705259675840210989546479265';
@@ -74,13 +66,13 @@ function Tests(ASSERT, KEM, MD, RSA, UTIL, JSBN, RANDOM) {
       var K = '0e6a26eb7b956ccb8b3bdc1ca975bc57c3989e8fbad31a224655d800c46954840ff32052cdf0d640562bdfadfa263cfccf3c52b29f2af4a1869959bc77f854cf15bd7a25192985a842dbff8e13efee5b7e7e55bbe4d389647c686a9a9ab3fb889b2d7767d3837eea4e0a2f04b53ca8f50fb31225c1be2d0126c8c7a4753b0807';
 
       var kdf = new KEM.kdf2(MD.sha1.create());
-
       var rnd = new FixedSecureRandom('032e45326fa859a72ec235acff929b15d1372e30b207255f0611b8f785d764374152e0ac009e509e7ba30cd2f1778e113b64e135cf4e2292c75efe5288edfda4');
-
       var kem = KEM.rsa.create(kdf, {rng: rnd});
 
-      var rsaPublicKey = RSA.setPublicKey(new JSBN.BigInteger(n), new JSBN.BigInteger(e));
-      var rsaPrivateKey = RSA.setPrivateKey(new JSBN.BigInteger(n),null , new JSBN.BigInteger(d));
+      var rsaPublicKey = RSA.setPublicKey(
+        new JSBN.BigInteger(n), new JSBN.BigInteger(e));
+      var rsaPrivateKey = RSA.setPrivateKey(
+        new JSBN.BigInteger(n), null, new JSBN.BigInteger(d));
 
       var result = kem.encrypt(rsaPublicKey, 128);
       ASSERT.equal(UTIL.bytesToHex(result.ciphertext), C0);
@@ -88,11 +80,9 @@ function Tests(ASSERT, KEM, MD, RSA, UTIL, JSBN, RANDOM) {
 
       var decryptedKey = kem.decrypt(rsaPrivateKey, result.ciphertext, 128);
       ASSERT.equal(UTIL.bytesToHex(decryptedKey), K);
-
     });
 
     it('should pass test vector C.6.3', function() {
-
       var n = '5888113332502691251761936431009284884966640757179802337490546478326238537107326596800820237597139824869184990638749556269785797065508097452399642780486933';
       var e = '65537';
       var d = '3202313555859948186315374524474173995679783580392140237044349728046479396037520308981353808895461806395564474639124525446044708705259675840210989546479265';
@@ -101,13 +91,13 @@ function Tests(ASSERT, KEM, MD, RSA, UTIL, JSBN, RANDOM) {
       var K = '09e2decf2a6e1666c2f6071ff4298305e2643fd510a2403db42a8743cb989de86e668d168cbe604611ac179f819a3d18412e9eb45668f2923c087c12fee0c5a0d2a8aa70185401fbbd99379ec76c663e875a60b4aacb1319fa11c3365a8b79a44669f26fb555c80391847b05eca1cb5cf8c2d531448d33fbaca19f6410ee1fcb';
 
       var kdf = new KEM.kdf1(MD.sha256.create(), 20);
-
       var rnd = new FixedSecureRandom('032e45326fa859a72ec235acff929b15d1372e30b207255f0611b8f785d764374152e0ac009e509e7ba30cd2f1778e113b64e135cf4e2292c75efe5288edfda4');
-
       var kem = KEM.rsa.create(kdf, {rng: rnd});
 
-      var rsaPublicKey = RSA.setPublicKey(new JSBN.BigInteger(n), new JSBN.BigInteger(e));
-      var rsaPrivateKey = RSA.setPrivateKey(new JSBN.BigInteger(n),null , new JSBN.BigInteger(d));
+      var rsaPublicKey = RSA.setPublicKey(
+        new JSBN.BigInteger(n), new JSBN.BigInteger(e));
+      var rsaPrivateKey = RSA.setPrivateKey(
+        new JSBN.BigInteger(n),null , new JSBN.BigInteger(d));
 
       var result = kem.encrypt(rsaPublicKey, 128);
       ASSERT.equal(UTIL.bytesToHex(result.ciphertext), C0);
@@ -115,11 +105,9 @@ function Tests(ASSERT, KEM, MD, RSA, UTIL, JSBN, RANDOM) {
 
       var decryptedKey = kem.decrypt(rsaPrivateKey, result.ciphertext, 128);
       ASSERT.equal(UTIL.bytesToHex(decryptedKey), K);
-
     });
 
     it('should pass test vector C.6.4', function() {
-
       var n = '5888113332502691251761936431009284884966640757179802337490546478326238537107326596800820237597139824869184990638749556269785797065508097452399642780486933';
       var e = '65537';
       var d = '3202313555859948186315374524474173995679783580392140237044349728046479396037520308981353808895461806395564474639124525446044708705259675840210989546479265';
@@ -128,13 +116,13 @@ function Tests(ASSERT, KEM, MD, RSA, UTIL, JSBN, RANDOM) {
       var K = '10a2403db42a8743cb989de86e668d168cbe604611ac179f819a3d18412e9eb45668f2923c087c12fee0c5a0d2a8aa70185401fbbd99379ec76c663e875a60b4aacb1319fa11c3365a8b79a44669f26fb555c80391847b05eca1cb5cf8c2d531448d33fbaca19f6410ee1fcb260892670e0814c348664f6a7248aaf998a3acc6';
 
       var kdf = new KEM.kdf2(MD.sha256.create(), 20);
-
       var rnd = new FixedSecureRandom('032e45326fa859a72ec235acff929b15d1372e30b207255f0611b8f785d764374152e0ac009e509e7ba30cd2f1778e113b64e135cf4e2292c75efe5288edfda4');
-
       var kem = KEM.rsa.create(kdf, {rng: rnd});
 
-      var rsaPublicKey = RSA.setPublicKey(new JSBN.BigInteger(n), new JSBN.BigInteger(e));
-      var rsaPrivateKey = RSA.setPrivateKey(new JSBN.BigInteger(n), null, new JSBN.BigInteger(d));
+      var rsaPublicKey = RSA.setPublicKey(
+        new JSBN.BigInteger(n), new JSBN.BigInteger(e));
+      var rsaPrivateKey = RSA.setPrivateKey(
+        new JSBN.BigInteger(n), null, new JSBN.BigInteger(d));
 
       var result = kem.encrypt(rsaPublicKey, 128);
       ASSERT.equal(UTIL.bytesToHex(result.ciphertext), C0);
@@ -142,7 +130,6 @@ function Tests(ASSERT, KEM, MD, RSA, UTIL, JSBN, RANDOM) {
 
       var decryptedKey = kem.decrypt(rsaPrivateKey, result.ciphertext, 128);
       ASSERT.equal(UTIL.bytesToHex(decryptedKey), K);
-
     });
   });
 }
