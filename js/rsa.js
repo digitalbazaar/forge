eaeade4d2c122fb2d296055ae9d72e12047b52f5
Dave Longley <dlongley@digitalbazaar.com>
2014-09-20 15:54:32 -0400

    Some code reorganization.
    - Move utility functions to bottom of file.
    - Use encode and decode functions for EMSA.

--- js/rsa.js
+++ js/rsa.js
@@ -296,191 +296,6 @@ var publicKeyValidator = forge.pki.rsa.publicKeyValidator = {
 };
 
 /**
- * Wrap digest in DigestInfo object.
- *
- * This function implements EMSA-PKCS1-v1_5-ENCODE as per RFC 3447.
- *
- * DigestInfo ::= SEQUENCE {
- *   digestAlgorithm DigestAlgorithmIdentifier,
- *   digest Digest
- * }
- *
- * DigestAlgorithmIdentifier ::= AlgorithmIdentifier
- * Digest ::= OCTET STRING
- *
- * @param md the message digest object with the hash to sign.
- *
- * @return the encoded message (ready for RSA encrytion)
- */
-var emsaPkcs1v15encode = function(md) {
-  // get the oid for the algorithm
-  var oid;
-  if(md.algorithm in pki.oids) {
-    oid = pki.oids[md.algorithm];
-  } else {
-    var error = new Error('Unknown message digest algorithm.');
-    error.algorithm = md.algorithm;
-    throw error;
-  }
-
-  // create the digest info
-  var digestInfo = asn1.create(
-    asn1.Class.UNIVERSAL, asn1.Type.SEQUENCE, true, []);
-  var digestAlgorithm = asn1.create(
-    asn1.Class.UNIVERSAL, asn1.Type.SEQUENCE, true, []);
-  digestAlgorithm.value.push(asn1.create(
-    asn1.Class.UNIVERSAL, asn1.Type.OID, false, oid));
-  digestAlgorithm.value.push(asn1.create(
-    asn1.Class.UNIVERSAL, asn1.Type.NULL, false, null));
-  var digest = asn1.create(
-    asn1.Class.UNIVERSAL, asn1.Type.OCTETSTRING, false, md.digest());
-  digestInfo.value.push(digestAlgorithm);
-  digestInfo.value.push(digest);
-
-  // encode digest info
-  return asn1.toDer(digestInfo).getBytes();
-};
-
-/**
- * Performs x^c mod n (RSA encryption or decryption operation).
- *
- * @param x the number to raise and mod.
- * @param key the key to use.
- * @param pub true if the key is public, false if private.
- *
- * @return the result of x^c mod n.
- */
-var _modPow = function(x, key, pub) {
-  if(pub) {
-    return x.modPow(key.e, key.n);
-  }
-
-  if(!key.p || !key.q) {
-    // allow calculation without CRT params (slow)
-    return x.modPow(key.d, key.n);
-  }
-
-  // pre-compute dP, dQ, and qInv if necessary
-  if(!key.dP) {
-    key.dP = key.d.mod(key.p.subtract(BigInteger.ONE));
-  }
-  if(!key.dQ) {
-    key.dQ = key.d.mod(key.q.subtract(BigInteger.ONE));
-  }
-  if(!key.qInv) {
-    key.qInv = key.q.modInverse(key.p);
-  }
-
-  /* Chinese remainder theorem (CRT) states:
-
-    Suppose n1, n2, ..., nk are positive integers which are pairwise
-    coprime (n1 and n2 have no common factors other than 1). For any
-    integers x1, x2, ..., xk there exists an integer x solving the
-    system of simultaneous congruences (where ~= means modularly
-    congruent so a ~= b mod n means a mod n = b mod n):
-
-    x ~= x1 mod n1
-    x ~= x2 mod n2
-    ...
-    x ~= xk mod nk
-
-    This system of congruences has a single simultaneous solution x
-    between 0 and n - 1. Furthermore, each xk solution and x itself
-    is congruent modulo the product n = n1*n2*...*nk.
-    So x1 mod n = x2 mod n = xk mod n = x mod n.
-
-    The single simultaneous solution x can be solved with the following
-    equation:
-
-    x = sum(xi*ri*si) mod n where ri = n/ni and si = ri^-1 mod ni.
-
-    Where x is less than n, xi = x mod ni.
-
-    For RSA we are only concerned with k = 2. The modulus n = pq, where
-    p and q are coprime. The RSA decryption algorithm is:
-
-    y = x^d mod n
-
-    Given the above:
-
-    x1 = x^d mod p
-    r1 = n/p = q
-    s1 = q^-1 mod p
-    x2 = x^d mod q
-    r2 = n/q = p
-    s2 = p^-1 mod q
-
-    So y = (x1r1s1 + x2r2s2) mod n
-         = ((x^d mod p)q(q^-1 mod p) + (x^d mod q)p(p^-1 mod q)) mod n
-
-    According to Fermat's Little Theorem, if the modulus P is prime,
-    for any integer A not evenly divisible by P, A^(P-1) ~= 1 mod P.
-    Since A is not divisible by P it follows that if:
-    N ~= M mod (P - 1), then A^N mod P = A^M mod P. Therefore:
-
-    A^N mod P = A^(M mod (P - 1)) mod P. (The latter takes less effort
-    to calculate). In order to calculate x^d mod p more quickly the
-    exponent d mod (p - 1) is stored in the RSA private key (the same
-    is done for x^d mod q). These values are referred to as dP and dQ
-    respectively. Therefore we now have:
-
-    y = ((x^dP mod p)q(q^-1 mod p) + (x^dQ mod q)p(p^-1 mod q)) mod n
-
-    Since we'll be reducing x^dP by modulo p (same for q) we can also
-    reduce x by p (and q respectively) before hand. Therefore, let
-
-    xp = ((x mod p)^dP mod p), and
-    xq = ((x mod q)^dQ mod q), yielding:
-
-    y = (xp*q*(q^-1 mod p) + xq*p*(p^-1 mod q)) mod n
-
-    This can be further reduced to a simple algorithm that only
-    requires 1 inverse (the q inverse is used) to be used and stored.
-    The algorithm is called Garner's algorithm. If qInv is the
-    inverse of q, we simply calculate:
-
-    y = (qInv*(xp - xq) mod p) * q + xq
-
-    However, there are two further complications. First, we need to
-    ensure that xp > xq to prevent signed BigIntegers from being used
-    so we add p until this is true (since we will be mod'ing with
-    p anyway). Then, there is a known timing attack on algorithms
-    using the CRT. To mitigate this risk, "cryptographic blinding"
-    should be used. This requires simply generating a random number r between
-    0 and n-1 and its inverse and multiplying x by r^e before calculating y
-    and then multiplying y by r^-1 afterwards.
-  */
-
-  // cryptographic blinding
-  var r;
-  do {
-    r = new BigInteger(
-      forge.util.bytesToHex(forge.random.getBytes(key.n.bitLength() / 8)),
-      16).mod(key.n);
-  } while(r.equals(BigInteger.ZERO));
-  x = x.multiply(r.modPow(key.e, key.n)).mod(key.n);
-
-  // calculate xp and xq
-  var xp = x.mod(key.p).modPow(key.dP, key.p);
-  var xq = x.mod(key.q).modPow(key.dQ, key.q);
-
-  // xp must be larger than xq to avoid signed bit usage
-  while(xp.compareTo(xq) < 0) {
-    xp = xp.add(key.p);
-  }
-
-  // do last step
-  var y = xp.subtract(xq)
-    .multiply(key.qInv).mod(key.p)
-    .multiply(key.q).add(xq);
-
-  // remove effect of random for cryptographic blinding
-  y = y.multiply(r.modInverse(key.n)).mod(key.n);
-
-  return y;
-};
-
-/**
  * NOTE: THIS METHOD IS DEPRECATED, use 'sign' on a private key object or
  * 'encrypt' on a public key object instead.
  *
@@ -1027,10 +842,10 @@ pki.setRsaPublicKey = pki.rsa.setPublicKey = function(n, e) {
          verify: function(digest, d) {
            // remove padding
            d = _decodePkcs1_v1_5(d, key, true);
-           // d is ASN.1 BER-encoded DigestInfo
-           var obj = asn1.fromDer(d);
-           // compare the given digest to the decrypted one
-           return digest === obj.value[1].value.bytes();
+           // decode
+           d = _decodeEmsaPkcs1_v1_5(d);
+           // FIXME: digest to become a ByteBuffer, use ByteBuffer.equals?
+           return d.getBytes() === digest;
          }
        };
      } else if(scheme === 'NONE' || scheme === 'NULL' || scheme === null) {
@@ -1039,7 +854,7 @@ pki.setRsaPublicKey = pki.rsa.setPublicKey = function(n, e) {
            // remove padding
            d = _decodePkcs1_v1_5(d, key, true);
            // FIXME: digest to become a ByteBuffer, use ByteBuffer.equals?
-           return digest === d.getBytes();
+           return d.getBytes() === digest;
          }
        };
      }
@@ -1159,10 +974,10 @@ pki.setRsaPrivateKey = pki.rsa.setPrivateKey = function(
     }
 
     if(scheme === undefined || scheme === 'RSASSA-PKCS1-V1_5') {
-      scheme = { encode: emsaPkcs1v15encode };
+      scheme = {encode: _encodeEmsaPkcs1_v1_5};
       bt = 0x01;
     } else if(scheme === 'NONE' || scheme === 'NULL' || scheme === null) {
-      scheme = { encode: function() { return md; } };
+      scheme = {encode: function() {return md;}};
       bt = 0x01;
     }
 
@@ -1371,6 +1186,200 @@ pki.publicKeyToRSAPublicKey = function(key) {
       _bnToBuffer(key.e))
   ]);
 };
+
+/**
+ * Performs x^c mod n (RSA encryption or decryption operation).
+ *
+ * @param x the number to raise and mod.
+ * @param key the key to use.
+ * @param pub true if the key is public, false if private.
+ *
+ * @return the result of x^c mod n.
+ */
+function _modPow(x, key, pub) {
+  if(pub) {
+    return x.modPow(key.e, key.n);
+  }
+
+  if(!key.p || !key.q) {
+    // allow calculation without CRT params (slow)
+    return x.modPow(key.d, key.n);
+  }
+
+  // pre-compute dP, dQ, and qInv if necessary
+  if(!key.dP) {
+    key.dP = key.d.mod(key.p.subtract(BigInteger.ONE));
+  }
+  if(!key.dQ) {
+    key.dQ = key.d.mod(key.q.subtract(BigInteger.ONE));
+  }
+  if(!key.qInv) {
+    key.qInv = key.q.modInverse(key.p);
+  }
+
+  /* Chinese remainder theorem (CRT) states:
+
+    Suppose n1, n2, ..., nk are positive integers which are pairwise
+    coprime (n1 and n2 have no common factors other than 1). For any
+    integers x1, x2, ..., xk there exists an integer x solving the
+    system of simultaneous congruences (where ~= means modularly
+    congruent so a ~= b mod n means a mod n = b mod n):
+
+    x ~= x1 mod n1
+    x ~= x2 mod n2
+    ...
+    x ~= xk mod nk
+
+    This system of congruences has a single simultaneous solution x
+    between 0 and n - 1. Furthermore, each xk solution and x itself
+    is congruent modulo the product n = n1*n2*...*nk.
+    So x1 mod n = x2 mod n = xk mod n = x mod n.
+
+    The single simultaneous solution x can be solved with the following
+    equation:
+
+    x = sum(xi*ri*si) mod n where ri = n/ni and si = ri^-1 mod ni.
+
+    Where x is less than n, xi = x mod ni.
+
+    For RSA we are only concerned with k = 2. The modulus n = pq, where
+    p and q are coprime. The RSA decryption algorithm is:
+
+    y = x^d mod n
+
+    Given the above:
+
+    x1 = x^d mod p
+    r1 = n/p = q
+    s1 = q^-1 mod p
+    x2 = x^d mod q
+    r2 = n/q = p
+    s2 = p^-1 mod q
+
+    So y = (x1r1s1 + x2r2s2) mod n
+         = ((x^d mod p)q(q^-1 mod p) + (x^d mod q)p(p^-1 mod q)) mod n
+
+    According to Fermat's Little Theorem, if the modulus P is prime,
+    for any integer A not evenly divisible by P, A^(P-1) ~= 1 mod P.
+    Since A is not divisible by P it follows that if:
+    N ~= M mod (P - 1), then A^N mod P = A^M mod P. Therefore:
+
+    A^N mod P = A^(M mod (P - 1)) mod P. (The latter takes less effort
+    to calculate). In order to calculate x^d mod p more quickly the
+    exponent d mod (p - 1) is stored in the RSA private key (the same
+    is done for x^d mod q). These values are referred to as dP and dQ
+    respectively. Therefore we now have:
+
+    y = ((x^dP mod p)q(q^-1 mod p) + (x^dQ mod q)p(p^-1 mod q)) mod n
+
+    Since we'll be reducing x^dP by modulo p (same for q) we can also
+    reduce x by p (and q respectively) before hand. Therefore, let
+
+    xp = ((x mod p)^dP mod p), and
+    xq = ((x mod q)^dQ mod q), yielding:
+
+    y = (xp*q*(q^-1 mod p) + xq*p*(p^-1 mod q)) mod n
+
+    This can be further reduced to a simple algorithm that only
+    requires 1 inverse (the q inverse is used) to be used and stored.
+    The algorithm is called Garner's algorithm. If qInv is the
+    inverse of q, we simply calculate:
+
+    y = (qInv*(xp - xq) mod p) * q + xq
+
+    However, there are two further complications. First, we need to
+    ensure that xp > xq to prevent signed BigIntegers from being used
+    so we add p until this is true (since we will be mod'ing with
+    p anyway). Then, there is a known timing attack on algorithms
+    using the CRT. To mitigate this risk, "cryptographic blinding"
+    should be used. This requires simply generating a random number r between
+    0 and n-1 and its inverse and multiplying x by r^e before calculating y
+    and then multiplying y by r^-1 afterwards.
+  */
+
+  // cryptographic blinding
+  var r;
+  do {
+    r = new BigInteger(
+      forge.util.bytesToHex(forge.random.getBytes(key.n.bitLength() / 8)),
+      16).mod(key.n);
+  } while(r.equals(BigInteger.ZERO));
+  x = x.multiply(r.modPow(key.e, key.n)).mod(key.n);
+
+  // calculate xp and xq
+  var xp = x.mod(key.p).modPow(key.dP, key.p);
+  var xq = x.mod(key.q).modPow(key.dQ, key.q);
+
+  // xp must be larger than xq to avoid signed bit usage
+  while(xp.compareTo(xq) < 0) {
+    xp = xp.add(key.p);
+  }
+
+  // do last step
+  var y = xp.subtract(xq)
+    .multiply(key.qInv).mod(key.p)
+    .multiply(key.q).add(xq);
+
+  // remove effect of random for cryptographic blinding
+  y = y.multiply(r.modInverse(key.n)).mod(key.n);
+
+  return y;
+}
+
+/**
+ * Wrap digest in DigestInfo object.
+ *
+ * This function implements EMSA-PKCS1-v1_5-ENCODE as per RFC 3447.
+ *
+ * DigestInfo ::= SEQUENCE {
+ *   digestAlgorithm DigestAlgorithmIdentifier,
+ *   digest Digest
+ * }
+ *
+ * DigestAlgorithmIdentifier ::= AlgorithmIdentifier
+ * Digest ::= OCTET STRING
+ *
+ * @param md the message digest object with the hash to sign.
+ *
+ * @return the encoded message (ready for RSA encrytion)
+ */
+function _encodeEmsaPkcs1_v1_5(md) {
+  // get the oid for the algorithm
+  var oid;
+  if(md.algorithm in pki.oids) {
+    oid = pki.oids[md.algorithm];
+  } else {
+    var error = new Error('Unknown message digest algorithm.');
+    error.algorithm = md.algorithm;
+    throw error;
+  }
+
+  // create the digest info
+  var digestInfo = asn1.create(
+    asn1.Class.UNIVERSAL, asn1.Type.SEQUENCE, true, []);
+  var digestAlgorithm = asn1.create(
+    asn1.Class.UNIVERSAL, asn1.Type.SEQUENCE, true, []);
+  digestAlgorithm.value.push(asn1.create(
+    asn1.Class.UNIVERSAL, asn1.Type.OID, false, oid));
+  digestAlgorithm.value.push(asn1.create(
+    asn1.Class.UNIVERSAL, asn1.Type.NULL, false, null));
+  var digest = asn1.create(
+    asn1.Class.UNIVERSAL, asn1.Type.OCTETSTRING, false, md.digest());
+  digestInfo.value.push(digestAlgorithm);
+  digestInfo.value.push(digest);
+
+  // encode digest info
+  return asn1.toDer(digestInfo).getBytes();
+}
+
+function _decodeEmsaPkcs1_v1_5(encoded) {
+  // encoded is ASN.1 BER-encoded DigestInfo
+  var obj = asn1.fromDer(encoded);
+  // TODO: validate DigestInfo
+
+  // return message digest
+  return obj.value[1].value;
+}
 
 /**
  * Encodes a message using PKCS#1 v1.5 padding.
