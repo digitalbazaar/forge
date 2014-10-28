e8aec428b811c4710284d89f5a2edc5fc9744152
Dave Longley <dlongley@digitalbazaar.com>
2014-10-28 10:35:08 -0400

    Refactor/consolidate tls test code.
    - WIP.

--- nodejs/test/tls.js
+++ nodejs/test/tls.js
@@ -4,6 +4,164 @@ function Tests(ASSERT, forge) {
   var ByteBuffer = forge.util.ByteBuffer;
 
   describe('tls', function() {
+    var clientSessionCache = forge.tls.createSessionCache();
+    var serverSessionCache = forge.tls.createSessionCache();
+
+    function createCertificate(cn, data) {
+      var keys = forge.pki.rsa.generateKeyPair(512);
+      var cert = forge.pki.createCertificate();
+      cert.publicKey = keys.publicKey;
+      cert.serialNumber = '01';
+      cert.validity.notBefore = new Date();
+      cert.validity.notAfter = new Date();
+      cert.validity.notAfter.setFullYear(
+        cert.validity.notBefore.getFullYear() + 1);
+      var attrs = [{
+        name: 'commonName',
+        value: cn
+      }, {
+        name: 'countryName',
+        value: 'US'
+      }, {
+        shortName: 'ST',
+        value: 'Virginia'
+      }, {
+        name: 'localityName',
+        value: 'Blacksburg'
+      }, {
+        name: 'organizationName',
+        value: 'Test'
+      }, {
+        shortName: 'OU',
+        value: 'Test'
+      }];
+      cert.setSubject(attrs);
+      cert.setIssuer(attrs);
+      cert.setExtensions([{
+        name: 'basicConstraints',
+        cA: true
+      }, {
+        name: 'keyUsage',
+        keyCertSign: true,
+        digitalSignature: true,
+        nonRepudiation: true,
+        keyEncipherment: true,
+        dataEncipherment: true
+      }, {
+        name: 'subjectAltName',
+        altNames: [{
+          type: 6, // URI
+          value: 'https://myuri.com/webid#me'
+        }]
+      }]);
+      cert.sign(keys.privateKey);
+      data[cn] = {
+        cert: forge.pki.certificateToPem(cert),
+        privateKey: forge.pki.privateKeyToPem(keys.privateKey)
+      };
+    }
+
+    function createClientConnection(options, callback) {
+      var end = options.end;
+      var data = options.data;
+      var assert = options.assert || {};
+      return forge.tls.createConnection({
+        server: false,
+        caStore: [data.server.cert],
+        sessionCache: {},
+        cipherSuites: [
+          forge.tls.CipherSuites.TLS_RSA_WITH_AES_128_CBC_SHA,
+          forge.tls.CipherSuites.TLS_RSA_WITH_AES_256_CBC_SHA],
+        virtualHost: 'server',
+        verify: function(c, verified, depth, certs) {
+          data.client.connection.commonName =
+            certs[0].subject.getField('CN').value;
+          data.client.connection.certVerified = verified;
+          return true;
+        },
+        connected: function(c) {
+          if(assert.version) {
+            ASSERT.equal(c.version.major, assert.version.major);
+            ASSERT.equal(c.version.minor, assert.version.minor);
+          }
+          c.prepare(new ByteBuffer('Hello Server', 'utf8'));
+        },
+        getCertificate: function(c, hint) {
+          return data.client.cert;
+        },
+        getPrivateKey: function(c, cert) {
+          return data.client.privateKey;
+        },
+        tlsDataReady: function(c) {
+          end.server.process(c.tlsData);
+        },
+        dataReady: function(c) {
+          data.client.connection.data = c.data.getBytes();
+          c.close();
+        },
+        closed: function(c) {
+          ASSERT.equal(data.client.connection.commonName, 'server');
+          ASSERT.equal(data.client.connection.certVerified, true);
+          ASSERT.equal(data.client.connection.data, 'Hello Client');
+          callback();
+        },
+        error: function(c, error) {
+          ASSERT.equal(error.message, undefined);
+        }
+      });
+    }
+
+    function createServerConnection(options) {
+      var end = options.end;
+      var data = options.data;
+      var assert = options.assert || {};
+      return forge.tls.createConnection({
+        server: true,
+        caStore: [data.client.cert],
+        sessionCache: {},
+        cipherSuites: [
+          forge.tls.CipherSuites.TLS_RSA_WITH_AES_128_CBC_SHA,
+          forge.tls.CipherSuites.TLS_RSA_WITH_AES_256_CBC_SHA],
+        connected: function(c) {
+          if(assert.version) {
+            ASSERT.equal(c.version.major, assert.version.major);
+            ASSERT.equal(c.version.minor, assert.version.minor);
+          }
+        },
+        verifyClient: true,
+        verify: function(c, verified, depth, certs) {
+          data.server.connection.commonName =
+            certs[0].subject.getField('CN').value;
+          data.server.connection.certVerified = verified;
+          return true;
+        },
+        getCertificate: function(c, hint) {
+          data.server.connection.certHint = hint[0];
+          return data.server.cert;
+        },
+        getPrivateKey: function(c, cert) {
+          return data.server.privateKey;
+        },
+        tlsDataReady: function(c) {
+          end.client.process(c.tlsData);
+        },
+        dataReady: function(c) {
+          data.server.connection.data = c.data.getBytes();
+          c.prepare(new ByteBuffer('Hello Client', 'utf8'));
+          c.close();
+        },
+        closed: function(c) {
+          ASSERT.equal(data.server.connection.certHint, 'server');
+          ASSERT.equal(data.server.connection.commonName, 'client');
+          ASSERT.equal(data.server.connection.certVerified, true);
+          ASSERT.equal(data.server.connection.data, 'Hello Server');
+        },
+        error: function(c, error) {
+          ASSERT.equal(error.message, undefined);
+        }
+      });
+    }
+
     it('should test TLS 1.0 PRF', function() {
       // Note: This test vector is originally from:
       // http://www.imc.org/ietf-tls/mail-archive/msg01589.html
@@ -30,149 +188,22 @@ function Tests(ASSERT, forge) {
       data.client.connection = {};
       data.server.connection = {};
 
-      end.client = forge.tls.createConnection({
-        server: false,
-        caStore: [data.server.cert],
-        sessionCache: {},
-        cipherSuites: [
-          forge.tls.CipherSuites.TLS_RSA_WITH_AES_128_CBC_SHA,
-          forge.tls.CipherSuites.TLS_RSA_WITH_AES_256_CBC_SHA],
-        virtualHost: 'server',
-        verify: function(c, verified, depth, certs) {
-          data.client.connection.commonName =
-            certs[0].subject.getField('CN').value;
-          data.client.connection.certVerified = verified;
-          return true;
-        },
-        connected: function(c) {
-          ASSERT.equal(c.version.major, 3);
-          ASSERT.equal(c.version.minor, 1);
-          c.prepare(new ByteBuffer('Hello Server', 'utf8'));
-        },
-        getCertificate: function(c, hint) {
-          return data.client.cert;
-        },
-        getPrivateKey: function(c, cert) {
-          return data.client.privateKey;
-        },
-        tlsDataReady: function(c) {
-          end.server.process(c.tlsData);
-        },
-        dataReady: function(c) {
-          data.client.connection.data = c.data.getBytes();
-          c.close();
-        },
-        closed: function(c) {
-          ASSERT.equal(data.client.connection.commonName, 'server');
-          ASSERT.equal(data.client.connection.certVerified, true);
-          ASSERT.equal(data.client.connection.data, 'Hello Client');
-          done();
-        },
-        error: function(c, error) {
-          ASSERT.equal(error.message, undefined);
-        }
-      });
-
-      end.server = forge.tls.createConnection({
-        server: true,
-        caStore: [data.client.cert],
-        sessionCache: {},
-        cipherSuites: [
-          forge.tls.CipherSuites.TLS_RSA_WITH_AES_128_CBC_SHA,
-          forge.tls.CipherSuites.TLS_RSA_WITH_AES_256_CBC_SHA],
-        connected: function(c) {
-          ASSERT.equal(c.version.major, 3);
-          ASSERT.equal(c.version.minor, 1);
-        },
-        verifyClient: true,
-        verify: function(c, verified, depth, certs) {
-          data.server.connection.commonName =
-            certs[0].subject.getField('CN').value;
-          data.server.connection.certVerified = verified;
-          return true;
-        },
-        getCertificate: function(c, hint) {
-          data.server.connection.certHint = hint[0];
-          return data.server.cert;
-        },
-        getPrivateKey: function(c, cert) {
-          return data.server.privateKey;
-        },
-        tlsDataReady: function(c) {
-          end.client.process(c.tlsData);
-        },
-        dataReady: function(c) {
-          data.server.connection.data = c.data.getBytes();
-          c.prepare(new ByteBuffer('Hello Client', 'utf8'));
-          c.close();
-        },
-        closed: function(c) {
-          ASSERT.equal(data.server.connection.certHint, 'server');
-          ASSERT.equal(data.server.connection.commonName, 'client');
-          ASSERT.equal(data.server.connection.certVerified, true);
-          ASSERT.equal(data.server.connection.data, 'Hello Server');
-        },
-        error: function(c, error) {
-          ASSERT.equal(error.message, undefined);
-        }
+      var assert = {
+        version: {major: 3, minor: 1}
+      };
+      end.client = createClientConnection({
+        end: end,
+        data: data,
+        assert: assert
+      }, done);
+      end.server = createServerConnection({
+        end: end,
+        data: data,
+        assert: assert
       });
 
       end.client.version = {major: 3, minor: 1};
       end.client.handshake();
-
-      function createCertificate(cn, data) {
-        var keys = forge.pki.rsa.generateKeyPair(512);
-        var cert = forge.pki.createCertificate();
-        cert.publicKey = keys.publicKey;
-        cert.serialNumber = '01';
-        cert.validity.notBefore = new Date();
-        cert.validity.notAfter = new Date();
-        cert.validity.notAfter.setFullYear(
-          cert.validity.notBefore.getFullYear() + 1);
-        var attrs = [{
-          name: 'commonName',
-          value: cn
-        }, {
-          name: 'countryName',
-          value: 'US'
-        }, {
-          shortName: 'ST',
-          value: 'Virginia'
-        }, {
-          name: 'localityName',
-          value: 'Blacksburg'
-        }, {
-          name: 'organizationName',
-          value: 'Test'
-        }, {
-          shortName: 'OU',
-          value: 'Test'
-        }];
-        cert.setSubject(attrs);
-        cert.setIssuer(attrs);
-        cert.setExtensions([{
-          name: 'basicConstraints',
-          cA: true
-        }, {
-          name: 'keyUsage',
-          keyCertSign: true,
-          digitalSignature: true,
-          nonRepudiation: true,
-          keyEncipherment: true,
-          dataEncipherment: true
-        }, {
-          name: 'subjectAltName',
-          altNames: [{
-            type: 6, // URI
-            value: 'https://myuri.com/webid#me'
-          }]
-        }]);
-        cert.sign(keys.privateKey);
-        data[cn] = {
-          cert: forge.pki.certificateToPem(cert),
-          privateKey: forge.pki.privateKeyToPem(keys.privateKey)
-        };
-      }
     });
 
     it('should establish a TLS 1.1 connection and transfer data', function(done) {
@@ -184,149 +215,22 @@ function Tests(ASSERT, forge) {
       data.client.connection = {};
       data.server.connection = {};
 
-      end.client = forge.tls.createConnection({
-        server: false,
-        caStore: [data.server.cert],
-        sessionCache: {},
-        cipherSuites: [
-          forge.tls.CipherSuites.TLS_RSA_WITH_AES_128_CBC_SHA,
-          forge.tls.CipherSuites.TLS_RSA_WITH_AES_256_CBC_SHA],
-        virtualHost: 'server',
-        verify: function(c, verified, depth, certs) {
-          data.client.connection.commonName =
-            certs[0].subject.getField('CN').value;
-          data.client.connection.certVerified = verified;
-          return true;
-        },
-        connected: function(c) {
-          ASSERT.equal(c.version.major, 3);
-          ASSERT.equal(c.version.minor, 2);
-          c.prepare(new ByteBuffer('Hello Server', 'utf8'));
-        },
-        getCertificate: function(c, hint) {
-          return data.client.cert;
-        },
-        getPrivateKey: function(c, cert) {
-          return data.client.privateKey;
-        },
-        tlsDataReady: function(c) {
-          end.server.process(c.tlsData);
-        },
-        dataReady: function(c) {
-          data.client.connection.data = c.data.getBytes();
-          c.close();
-        },
-        closed: function(c) {
-          ASSERT.equal(data.client.connection.commonName, 'server');
-          ASSERT.equal(data.client.connection.certVerified, true);
-          ASSERT.equal(data.client.connection.data, 'Hello Client');
-          done();
-        },
-        error: function(c, error) {
-          ASSERT.equal(error.message, undefined);
-        }
-      });
-
-      end.server = forge.tls.createConnection({
-        server: true,
-        caStore: [data.client.cert],
-        sessionCache: {},
-        cipherSuites: [
-          forge.tls.CipherSuites.TLS_RSA_WITH_AES_128_CBC_SHA,
-          forge.tls.CipherSuites.TLS_RSA_WITH_AES_256_CBC_SHA],
-        connected: function(c) {
-          ASSERT.equal(c.version.major, 3);
-          ASSERT.equal(c.version.minor, 2);
-        },
-        verifyClient: true,
-        verify: function(c, verified, depth, certs) {
-          data.server.connection.commonName =
-            certs[0].subject.getField('CN').value;
-          data.server.connection.certVerified = verified;
-          return true;
-        },
-        getCertificate: function(c, hint) {
-          data.server.connection.certHint = hint[0];
-          return data.server.cert;
-        },
-        getPrivateKey: function(c, cert) {
-          return data.server.privateKey;
-        },
-        tlsDataReady: function(c) {
-          end.client.process(c.tlsData);
-        },
-        dataReady: function(c) {
-          data.server.connection.data = c.data.getBytes();
-          c.prepare(new ByteBuffer('Hello Client', 'utf8'));
-          c.close();
-        },
-        closed: function(c) {
-          ASSERT.equal(data.server.connection.certHint, 'server');
-          ASSERT.equal(data.server.connection.commonName, 'client');
-          ASSERT.equal(data.server.connection.certVerified, true);
-          ASSERT.equal(data.server.connection.data, 'Hello Server');
-        },
-        error: function(c, error) {
-          ASSERT.equal(error.message, undefined);
-        }
+      var assert = {
+        version: {major: 3, minor: 2}
+      };
+      end.client = createClientConnection({
+        end: end,
+        data: data,
+        assert: assert
+      }, done);
+      end.server = createServerConnection({
+        end: end,
+        data: data,
+        assert: assert
       });
 
       end.client.version = {major: 3, minor: 2};
       end.client.handshake();
-
-      function createCertificate(cn, data) {
-        var keys = forge.pki.rsa.generateKeyPair(512);
-        var cert = forge.pki.createCertificate();
-        cert.publicKey = keys.publicKey;
-        cert.serialNumber = '01';
-        cert.validity.notBefore = new Date();
-        cert.validity.notAfter = new Date();
-        cert.validity.notAfter.setFullYear(
-          cert.validity.notBefore.getFullYear() + 1);
-        var attrs = [{
-          name: 'commonName',
-          value: cn
-        }, {
-          name: 'countryName',
-          value: 'US'
-        }, {
-          shortName: 'ST',
-          value: 'Virginia'
-        }, {
-          name: 'localityName',
-          value: 'Blacksburg'
-        }, {
-          name: 'organizationName',
-          value: 'Test'
-        }, {
-          shortName: 'OU',
-          value: 'Test'
-        }];
-        cert.setSubject(attrs);
-        cert.setIssuer(attrs);
-        cert.setExtensions([{
-          name: 'basicConstraints',
-          cA: true
-        }, {
-          name: 'keyUsage',
-          keyCertSign: true,
-          digitalSignature: true,
-          nonRepudiation: true,
-          keyEncipherment: true,
-          dataEncipherment: true
-        }, {
-          name: 'subjectAltName',
-          altNames: [{
-            type: 6, // URI
-            value: 'https://myuri.com/webid#me'
-          }]
-        }]);
-        cert.sign(keys.privateKey);
-        data[cn] = {
-          cert: forge.pki.certificateToPem(cert),
-          privateKey: forge.pki.privateKeyToPem(keys.privateKey)
-        };
-      }
     });
 
     // TODO: add session resumption test
