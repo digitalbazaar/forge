6a4679c534401ef6c5dff94ce5190142a41de703
Sunny <ratsunny@gmail.com>
2015-03-04 16:29:40 +0800

    Byte stream mode support on modes.ctr

--- js/cipherModes.js
+++ js/cipherModes.js
@@ -288,6 +288,7 @@ modes.ofb.prototype.decrypt = modes.ofb.prototype.encrypt;
 modes.ctr = function(options) {
   options = options || {};
   this.name = 'CTR';
+  this.byteStreamMode = true;
   this.cipher = options.cipher;
   this.blockSize = options.blockSize || 16;
   this._blocks = this.blockSize / 4;
@@ -302,30 +303,30 @@ modes.ctr.prototype.start = function(options) {
   // use IV as first input
   this._iv = transformIV(options.iv);
   this._inBlock = this._iv.slice(0);
+  this._cursor = this.blockSize;
 };
 
 modes.ctr.prototype.encrypt = function(input, output) {
-  // encrypt block (CTR always uses encryption mode)
-  this.cipher.encrypt(this._inBlock, this._outBlock);
+  // encrypt block while cursor moves to the end of the block
+  // (CTR always uses encryption mode)
+  if (this._cursor === this.blockSize) {
+    this._cursor = 0;
+    this.cipher.encrypt(this._inBlock, this._outBlock);
 
-  // increment counter (input block)
-  inc32(this._inBlock);
+    // increment counter (input block)
+    inc32(this._inBlock);
+  }
 
   // XOR input with output
-  for(var i = 0; i < this._blocks; ++i) {
-    output.putInt32(input.getInt32() ^ this._outBlock[i]);
+  var endPos = Math.min(this.blockSize - this._cursor, input.length()) + this._cursor;
+  for (var i = this._cursor, offset, result; i < endPos; ++i, ++this._cursor) {
+    offset = (3 - (i % 4)) * 8;
+    result = this._outBlock[i/4|0] & (0xFF << offset);
+    output.putByte(input.getByte() ^ (result >>> offset));
   }
 };
 
 modes.ctr.prototype.decrypt = modes.ctr.prototype.encrypt;
-
-modes.ctr.prototype.afterFinish = function(output, options) {
-  // handle stream mode truncation
-  if(options.overflow > 0) {
-    output.truncate(this.blockSize - options.overflow);
-  }
-  return true;
-};
 
 
 /** Galois/Counter Mode (GCM) **/
