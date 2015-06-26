3eb3b59694949e66f1cf578cbda153469d3af80e
Dave Longley <dlongley@digitalbazaar.com>
2015-06-26 17:07:16 -0400

    Disable date-based tests on phantomjs due to its date bugs.

--- nodejs/test/pkcs7.js
+++ nodejs/test/pkcs7.js
@@ -468,6 +468,13 @@ function Tests(ASSERT, PKCS7, PKI, AES, DES, UTIL) {
       ASSERT.equal(pem, _pem.signedDataNoAttrs);
     });
 
+    if(typeof define === 'function') {
+      // return here for phantomjs; it has date serialization and timezone
+      // bugs that prevent the following tests from running properly
+      // see: https://github.com/ariya/phantomjs/issues/11712
+      return;
+    }
+
     it('should create PKCS#7 SignedData with content-type, message-digest, ' +
       'and signing-time attributes using UTCTime', function() {
       // verify with:
