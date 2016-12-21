251ada5a26d6814abc2975198e33a580596c49d5
David I. Lehn <dlehn@digitalbazaar.com>
2016-12-20 20:58:21 -0500

    Refactor tests and examples.
    - Move files:
      - Examples to examples/.
      - Tests to tests/.
      - Test server to tests/.
      - Core unit tests to tests/unit/.
      - Legacy browser tests to tests/legacy/.
      - Benchmarks to tests/benchmarks/.
      - WebSockets related to tests/websockets/.
      - Issue related to tests/issues/.
      - Support files to root.
      - Test deps added to main package.json.
    - Update paths everywhere.
    - Built bundles:
      - Support building multiple configurations.
      - dist/forge.js: core forge.
      - dist/forge.all.js: core + extra utils + networking.
    - Update lib/form.js, lib/xhr.js to CommonJS.
    - Update READMEs.
    - Update test server with appropriate files.
    - Add primary test index.
    - Update legacy index and files for new layout.
    - Update tests to use single forge bundles.
    - Remove test trailing whitespace.
    - Turn off legacy test scrolling due to performance.

---
+++ webpack-tests.config.js
@@ -0,0 +1,27 @@
+/**
+ * Forge webpack build rules for unit tests.
+ *
+ * @author Digital Bazaar, Inc.
+ *
+ * Copyright 2011-2016 Digital Bazaar, Inc.
+ */
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    'forge-tests': './tests/unit/index.js'
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js',
+    library: '[name]',
+    libraryTarget: 'umd'
+  },
+  node: {
+    Buffer: false,
+    process: false,
+    crypto: false,
+    setImmediate: false
+  }
+};
