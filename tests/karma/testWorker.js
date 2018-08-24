47ec5f70a4891fc4d9058284862e816089594436
Leo Selig <dev.selig@gmail.com>
2018-08-24 10:01:53 +0200

    Fix code formatting issues
    These were mentioned in the PR review here:
    https://github.com/digitalbazaar/forge/pull/586#issuecomment-413004807

--- tests/karma/testWorker.js
+++ tests/karma/testWorker.js
@@ -6,30 +6,30 @@ var PKI = require('../../lib/pki');
 // We  define a light-weight protocol to simplify testing 
 self.addEventListener('message', function (event) {
       
-      // Test scripts call worker.postMessage(data)` – we can access the payload via event.data
+  // Test scripts call worker.postMessage(data)` – we can access the payload via event.data
 
-      // data.method defines what the worker should call (we skip sophisticated reflection-magic,
-      // argument passing, etc. for now)
-      switch (event.data.method) {
-            case 'rsa.generateKeyPair':
-                  RSA.generateKeyPair({ bits: 512, workers: -1 }, function(error, keyPair) {
-                        // We signal the outcome of the call via event.data with...
-                        if (error) {
-                              // ...event.data.type === 'error' if the call failed
-                              self.postMessage({
-                                    type: 'error',
-                                    result: error.toString()
-                              });
-                        } else {
-                              // ...event.data.type === 'success' if the call succeeded
-                              self.postMessage({
-                                    type: 'success',
-                                    result: {
-                                          publicKey: PKI.publicKeyToPem(keyPair.publicKey),
-                                          privateKey: PKI.privateKeyToPem(keyPair.privateKey),
-                                    }
-                              });
-                        }
-                  });
-      }
+  // data.method defines what the worker should call (we skip sophisticated reflection-magic,
+  // argument passing, etc. for now)
+  switch (event.data.method) {
+    case 'rsa.generateKeyPair':
+      RSA.generateKeyPair({ bits: 512, workers: -1 }, function(error, keyPair) {
+        // We signal the outcome of the call via event.data with...
+        if (error) {
+          // ...event.data.type === 'error' if the call failed
+          self.postMessage({
+            type: 'error',
+            result: error.toString()
+          });
+        } else {
+          // ...event.data.type === 'success' if the call succeeded
+          self.postMessage({
+            type: 'success',
+            result: {
+              publicKey: PKI.publicKeyToPem(keyPair.publicKey),
+              privateKey: PKI.privateKeyToPem(keyPair.privateKey),
+            }
+          });
+        }
+      });
+  }
 });
