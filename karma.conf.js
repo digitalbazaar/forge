dd484e37704e533b3fd6bc005d274c11fb56f52a
ossdev07 <ossdev@puresoftware.com>
2018-10-09 17:50:22 +0530

    Forge: Removed phantomjs dependency
    PhantomJS is an unmaintained project, removed
    phantomjs support from package.json.
    Changed browser to chromeheadless in karma.conf.js
    Updated jshint version to remove optional phantomjs
    dependency.
    
    Signed-off-by: ossdev07 <ossdev@puresoftware.com>

--- karma.conf.js
+++ karma.conf.js
@@ -86,7 +86,7 @@ module.exports = function(config) {
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     //browsers: ['PhantomJS', 'Chrome', 'Firefox', 'Safari'],
-    browsers: ['PhantomJS'],
+    browsers: ['ChromeHeadless'],
 
     customLaunchers: {
       IE9: {
