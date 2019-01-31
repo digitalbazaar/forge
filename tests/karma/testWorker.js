b241077218f7ef7f62e61bd95195e2fd9cc20f43
David I. Lehn <dlehn@digitalbazaar.com>
2019-01-31 18:41:58 -0500

    Improve web worker debug features.

--- tests/karma/testWorker.js
+++ tests/karma/testWorker.js
@@ -1,36 +1,65 @@
 var RSA = require('../../lib/rsa');
 var PKI = require('../../lib/pki');
 
+// log on main thread
+function _log(message) {
+  self.postMessage({
+    type: 'log',
+    message: message
+  });
+}
+
 // The following code allows main thread scripts to make the worker call
 // certain forge APIs within the worker (primarily to ensure compatibility)
 // We define a light-weight protocol to simplify testing
-self.addEventListener('message', function (event) {
-
+// @param event
+//          event.data {Object} message content
+//          event.data.type {String} message type
+//          event.data.* {Any} type specific data
+self.addEventListener('message', function(event) {
   // Test scripts call worker.postMessage(data)
   // we can access the payload via event.data
 
-  // data.method defines what the worker should call (we skip sophisticated
-  // reflection-magic, argument passing, etc. for now)
-  switch (event.data.method) {
+  //_log('message type: ' + event.data.type);
+
+  // data.type defines what the worker should call
+  switch(event.data.type) {
+    case 'ping':
+      self.postMessage({
+        type: 'pong'
+      });
+      break;
     case 'rsa.generateKeyPair':
-      RSA.generateKeyPair({ bits: 512, workers: -1 }, function(error, keyPair) {
+      //_log('keygen start');
+      //RSA.generateKeyPair({bits: 512, workers: -1}, function(error, keyPair) {
+      RSA.generateKeyPair({bits: 512, workers: 1}, function(error, keyPair) {
+        //_log('keygen done');
         // We signal the outcome of the call via event.data with...
-        if (error) {
+        if(error) {
           // ...event.data.type === 'error' if the call failed
           self.postMessage({
             type: 'error',
-            result: error.toString()
+            error: error.toString()
           });
         } else {
           // ...event.data.type === 'success' if the call succeeded
           self.postMessage({
             type: 'success',
-            result: {
+            keypair: {
               publicKey: PKI.publicKeyToPem(keyPair.publicKey),
-              privateKey: PKI.privateKeyToPem(keyPair.privateKey),
+              privateKey: PKI.privateKeyToPem(keyPair.privateKey)
             }
           });
         }
+      });
+      break;
+    case 'stop':
+      self.close();
+      break;
+    default:
+      self.postMessage({
+        type: 'error',
+        error: 'Unknown message type: ' + event.data.type
       });
   }
 });
--- tests/karma/web-worker-rsa.js
+++ tests/karma/web-worker-rsa.js
@@ -6,21 +6,50 @@ var ASSERT = require('assert');
 var TestWorker = require('worker-loader!./testWorker');
 var testWorker = new TestWorker();
 
-describe('web worker rsa', function() {
+function _log(message) {
+  console.log('[main] ' + message);
+}
+
+describe.only('web worker rsa', function() {
   it('should generate key pairs when running forge in a web worker', function(done) {
     // Make test worker call rsa.generateKeyPair() on its own side
-    testWorker.postMessage({ method: 'rsa.generateKeyPair' });
+    //testWorker.postMessage({type: 'ping'});
+    testWorker.postMessage({type: 'rsa.generateKeyPair'});
 
     // Wait for a result (see testWorker.js for what event data to expect)
-    testWorker.addEventListener('message', function (event) {
-      ASSERT.equal(event.data.type, 'success');
-      // This is primarily to ensure that the node-forge code runs successfully
-      // in the worker (e.g. no usages of `window` or similar). So, comparing
-      // for structural sanity of the result only should be fine
-      ASSERT.equal(typeof event.data.result.publicKey, 'string');
-      ASSERT.equal(typeof event.data.result.privateKey, 'string');
+    testWorker.addEventListener('message', function(event) {
+      //_log('message type: ' + event.data.type);
+      switch(event.data.type) {
+        case 'success':
+          // This is primarily to ensure that the node-forge code runs
+          // successfully in the worker (e.g. no usages of `window` or
+          // similar). So, comparing for structural sanity of the result only
+          // should be fine.
+          ASSERT.equal(typeof event.data.keypair, 'object');
+          ASSERT.equal(typeof event.data.keypair.publicKey, 'string');
+          ASSERT.equal(typeof event.data.keypair.privateKey, 'string');
 
-      done();
+          // done with worker tests
+          testWorker.terminate();
+
+          done();
+          break;
+        case 'error':
+          ASSERT.fail('web worker error: ' + event.data.result);
+          break;
+        case 'log':
+          console.log('[worker] ' + event.data.message);
+          break;
+        case 'pong':
+          // for debugging worker is alive
+          //_log('pong');
+          break;
+        case 'stop':
+          testWorker.terminate();
+          break;
+        default:
+          console.log('UNKNOWN MESSAGE TYPE: ' + event.data.type);
+      }
     });
   });
 });
