efae46a7337ee2c5340cc125cb8353c1dacecd8f
David I. Lehn <dlehn@digitalbazaar.com>
2019-01-31 01:37:02 -0500

    Formatting.

--- tests/karma/testWorker.js
+++ tests/karma/testWorker.js
@@ -1,15 +1,16 @@
 var RSA = require('../../lib/rsa');
 var PKI = require('../../lib/pki');
 
-// The following code allows main thread scripts to make the worker call certain forge APIs within
-// the worker (primarily to ensure compatibility)
-// We  define a light-weight protocol to simplify testing 
+// The following code allows main thread scripts to make the worker call
+// certain forge APIs within the worker (primarily to ensure compatibility)
+// We define a light-weight protocol to simplify testing
 self.addEventListener('message', function (event) {
-      
-  // Test scripts call worker.postMessage(data)` – we can access the payload via event.data
 
-  // data.method defines what the worker should call (we skip sophisticated reflection-magic,
-  // argument passing, etc. for now)
+  // Test scripts call worker.postMessage(data)
+  // we can access the payload via event.data
+
+  // data.method defines what the worker should call (we skip sophisticated
+  // reflection-magic, argument passing, etc. for now)
   switch (event.data.method) {
     case 'rsa.generateKeyPair':
       RSA.generateKeyPair({ bits: 512, workers: -1 }, function(error, keyPair) {
