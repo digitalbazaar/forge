8343452307c8483aa1cc1955e533c3e599c0dff5
David I. Lehn <dlehn@digitalbazaar.com>
2019-01-31 00:41:26 -0500

    Switch tests from PhantomJS to headless Chrome.
    - PhantomJS is deprecated. Now using headless Chrome.

--- tests/unit/support.js
+++
@@ -1,7 +0,0 @@
-// test support
-
-// true if running in PhantomJS
-exports.isPhantomJS =
-  (typeof navigator !== 'undefined' && navigator.userAgent) ?
-  navigator.userAgent.indexOf('PhantomJS') !== -1 :
-  false;
