3ade5afb41a9b1cd85a14a25c4745b6625af90f0
David I. Lehn <dlehn@digitalbazaar.com>
2019-01-31 01:37:02 -0500

    Formatting.

--- tests/karma/rsa.js
+++ tests/karma/rsa.js
@@ -1,11 +1,10 @@
 var ASSERT = require('assert');
 
-// The worker-loader build the ./testWorker.js as a separate bundle and returns a constructor
-// that saves us from having to know the public script path
+// The worker-loader build the ./testWorker.js as a separate bundle and returns
+// a constructor that saves us from having to know the public script path
 // I.e.: new Worker('path/to/public/script.js') becomes new TestWorker()
 var TestWorker = require('worker-loader!./testWorker');
 var testWorker = new TestWorker();
-
 
 describe('web worker rsa', function() {
   it('should generate key pairs when running forge in a web worker', function(done) {
@@ -15,9 +14,9 @@ describe('web worker rsa', function() {
     // Wait for a result (see testWorker.js for what event data to expect)
     testWorker.addEventListener('message', function (event) {
       ASSERT.equal(event.data.type, 'success');
-      // This is primarily to ensure that the node-forge code runs successfully in the worker
-      // (e.g. no usages of `window` or similar). So, comparing for structural sanity of the result
-      // only should be fine
+      // This is primarily to ensure that the node-forge code runs successfully
+      // in the worker (e.g. no usages of `window` or similar). So, comparing
+      // for structural sanity of the result only should be fine
       ASSERT.equal(typeof event.data.result.publicKey, 'string');
       ASSERT.equal(typeof event.data.result.privateKey, 'string');
 
