72346b94cb4b8cddca7e0154fb41cccdbfecf07d
David I. Lehn <dlehn@digitalbazaar.com>
2019-02-01 19:38:20 -0500

    Switch to eslint.
    - Remove jscs and jshint.
    - Use eslint-config-digitalbazaar.
    - Add overrides to handle current ~ES5 code.

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "node": true
+  },
+  "extends": ["eslint-config-digitalbazaar"],
+  "parserOptions": {
+    "ecmaVersion": 5
+  },
+  "rules": {
+    "quotes": [
+      "error",
+      "single"
+    ],
+    // overrides to support ES5, remove when updated to ES20xx
+    "no-unused-vars": "warn",
+    "no-var": "off",
+    "object-shorthand": "off",
+    "prefer-const": "off",
+  }
+};
